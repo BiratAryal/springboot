9d04cb82e254a40d90facc70a9ade5a304de4adb
Birat Aryal <E00335@ycosolutions.com>
2025-03-17 21:49:14 +0545

    Used MVC and updated the api page

--- src/main/java/com/birataryal/springapp/springappcontroller.java
+++ src/main/java/com/birataryal/springapp/springappcontroller.java
@@ -1,11 +1,12 @@
 package com.birataryal.springapp;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-
-@RestController
+@Controller
 public class springappcontroller {
     
     @Value("${spring.application.name}")
@@ -17,8 +18,18 @@ public class springappcontroller {
     @Value("${spring.profiles.active}")
     private String profile;
 
-    @RequestMapping("/api")
-    public String hello() {
-        return String.format("%s | Profile: %s | Port %s", appName,profile, port);
-    }
+    // @RequestMapping("/api")
+    // @GetMapping("/api")
+    // @ResponseBody
+    // public String hello() {
+    //     return String.format("%s | Profile: %s | Port %s", appName,profile, port);
+    // }
+
+    @GetMapping("/api")
+    public String greetings(Model model) {
+        model.addAttribute("appName", appName);
+        model.addAttribute("profile", profile);
+        model.addAttribute("port", port);
+        return "api";
+    }   
 }
